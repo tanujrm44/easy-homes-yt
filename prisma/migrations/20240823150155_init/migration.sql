dfbf86b7e55b6172354d33b3067eb1d36f8c76e7
Tanuj Malode <tanujmalode@Tanujs-MacBook-Air.local>
2024-08-23 22:01:03 +0530

    db setup with prisma

---
+++ prisma/migrations/20240823150155_init/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE `Images` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(191) NOT NULL,
+    `propertyId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Images_propertyId_key`(`propertyId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Property` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `type` ENUM('RENT', 'SALE') NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `street` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `zipcode` VARCHAR(191) NOT NULL,
+    `bhk` ENUM('ONE_RK', 'ONE_BHK', 'TWO_BHK', 'THREE_BHK', 'FOUR_BHK') NOT NULL,
+    `area` INTEGER NOT NULL,
+    `parking` ENUM('YES', 'NO') NOT NULL,
+    `preferredTenants` ENUM('FAMILY', 'BACHELORS') NOT NULL,
+    `protertyType` ENUM('APARTMENT', 'INDEPENDENT') NOT NULL,
+    `availableFrom` DATETIME(3) NOT NULL,
+    `isFeatured` BOOLEAN NOT NULL DEFAULT false,
+    `isSold` BOOLEAN NOT NULL DEFAULT false,
+    `ownerId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Images` ADD CONSTRAINT `Images_propertyId_fkey` FOREIGN KEY (`propertyId`) REFERENCES `Property`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Property` ADD CONSTRAINT `Property_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
