07a1ed9763a743e478cab89fe2a8b4167893b027
Tanuj Malode <tanujrm44@gmail.com>
2024-12-07 11:15:00 +0530

    test 5

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,22 @@
-export default function sitemap() {
+import { getProperties } from "@/actions"
+
+export default async function sitemap() {
+  const properties = await getProperties()
+
+  const propertyUrls = properties.map((p) => ({
+    url: `https://easy-homes-yt.vercel.app/properties1/${p.id}`,
+    lastModified: new Date(),
+  }))
   return [
     {
       url: "https://easy-homes-yt.vercel.app",
-      lastModified: new Date().toISOString(),
+      lastModified: new Date(),
     },
     {
       url: "https://easy-homes-yt.vercel.app/properties",
-      lastModified: new Date().toISOString(),
+      lastModified: new Date(),
     },
+
+    ...propertyUrls,
   ]
 }
